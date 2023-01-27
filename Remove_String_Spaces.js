4090de06b8f5ce188eed64957079aebf9cb87252
raghad-emad <raghademaad163@gmail.com>
2023-01-28 01:56:30 +0200

    Day 13

---
+++ Remove_String_Spaces.js
@@ -0,0 +1,11 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+
+let mystring = x.split(' ').join('');
+
+return mystring  
+
+}
+
+console.log(noSpace('w e l c o m e'));
