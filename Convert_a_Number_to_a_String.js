3c0a59c19c7d57decfae77f81874ff05dcfbda8c
raghad-emad <raghademaad163@gmail.com>
2023-02-03 18:09:26 +0200

    Day 16

---
+++ Convert_a_Number_to_a_String.js
@@ -0,0 +1,18 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+console.log(numberToString(22))
+
+
+
+
+
+// We need a function that can transform a string into a number.
+
+const stringToNumber = function(str){
+    return Number(str);
+  }
+console.log(stringToNumber('13'));
