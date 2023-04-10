d60a313ec0eeace19eaedaacd57bcc82e327cf01
raghad-emad <raghademaad163@gmail.com>
2023-04-10 09:23:30 +0200

    Day 27

---
+++ Find_The_First_Non_Consecutive_Number.js
@@ -0,0 +1,14 @@
+function firstNonConsecutive (arr) {
+
+    for (let i = 1; i < arr.length; i++) {
+
+            if (arr[i] - 1 != arr[i-1]) {
+                return arr[i]
+            }       
+        
+    }
+
+    return null;
+}
+
+console.log(firstNonConsecutive([1,2,3,4,6,7,9]))
